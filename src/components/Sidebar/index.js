ee11a0bf53d8735ac82d96b67048ba375ab0f574
Tinh-310 <trongtinhdao99@gmail.com>
2022-04-11 18:20:43 +0700

    layOut,sideBar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,63 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => (
+  <div className='nav-bar'>
+    <Link className='logo' to="/">
+      <img src={LogoS} alt="logo" />
+      <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+    </Link>
+    <nav>
+      <NavLink exact="true" Classname='active' to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+
+      <NavLink
+        exact="true"
+        Classname='active'
+        className='about-link'
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+
+      <NavLink
+        exact="true"
+        Classname='active'
+        className='contact-link'
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/t%C3%ADnh-%C4%91%C3%A0o-tr%E1%BB%8Dng-628560237/">
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://github.com/Tinh-310">
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://www.instagram.com/trongtinhdao99/">
+          <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://facebook.com/trongtinh310">
+          <FontAwesomeIcon icon={faFacebook} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Sidebar
